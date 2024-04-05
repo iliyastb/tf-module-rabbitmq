5a7dbe0ee0d74c6f0384cde1fa45a40290fc5783
iliyastb <skiliyas300@gmail.com>
2024-04-05 20:01:50 +0530

    Update

--- data.tf
+++ data.tf
@@ -3,3 +3,7 @@ data "aws_ami" "ami" {
   name_regex  = "centos8-ansible"
   owners      = ["self"]
 }
+
+data "aws_route53_zone" "domain" {
+  name = var.dns_domain
+}
--- vars.tf
+++ vars.tf
@@ -2,3 +2,7 @@ variable "env" {}
 variable "tags" {}
 variable "instance_type" {}
 variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+variable "dns_domain" {}
+variable "bastion_cidr" {}
